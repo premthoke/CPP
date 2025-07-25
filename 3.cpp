839cbc9616dd37f21f27a503b7cd013eb8de0f9b
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-25 22:38:50 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+// input output 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+int a;
+string b;
+cout<<"enter your name: ";
+cin>>b;
+cout<<"enter your age: ";
+cin>>a;
+cout<<endl<<"Hello "<<b<<" cheers to your "<<a<<endl;
+cout<<"enjoy your beautiful life buddy ";
+return 0;
+}
