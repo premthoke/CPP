2a1a2c2ca4c4dc7781c8a9771bfc245cbcd469c5
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-25 22:26:49 +0530

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+// Data types
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+int a=1;
+char b= 'b';
+float c= 7.11;
+double d=100.729;
+string e= "prem";
+cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e;
+}
