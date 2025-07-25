3211d0c5636a511e1b8cf7810b9be1afeb4385af
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-25 23:08:53 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,49 @@
 // Data types
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-int a=1;
-char b= 'b';
-float c= 7.11;
-double d=100.729;
-string e= "prem";
-cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e;
+int main()
+{
+    int a = 1;
+    char b = 'b';
+    float c = 7.11;
+    double d = 100.729;
+    string e = "prem";
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << d << endl
+         << e;
 }
+
+// operators 
+
+// Arithmetic-
+Adiition (+)
+Subtraction (-) 
+Multiplication (*)
+Division (/)
+  
+// Relational-
+less than (<) 
+greater than (>)
+less than equal to (<=) 
+greater than equal to (>=) 
+not equal to (!=) 
+equalto (==)
+  
+//Logical-
+AND (&&)
+OR (||)
+NOT (!)
+
+//Unary- 
+Increment ++
+Decrement --
+
+//Modulus
+ % 
+  
+  
+  
