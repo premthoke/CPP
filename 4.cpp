4a0fe6dedf75c2c1f7e04be82ff546cd71d44fa7
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-25 22:48:53 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+// Type casting - converting variables from one datatype to another one 
+// ASCII - char to num & num to char 
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x = 98;
+    char ch = 'a';
+    cout << (char)x;
+    cout << (int)ch;
+    return 0;
+}
