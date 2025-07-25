69bea431ddb29b484809eb040f9e80ad1221adb0
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-25 23:15:34 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,6 +1,5 @@
 // Type casting - converting variables from one datatype to another one 
 // ASCII - char to num & num to char 
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -10,5 +9,32 @@ int main()
     char ch = 'a';
     cout << (char)x;
     cout << (int)ch;
+    return 0;
+}
+
+//Pre Increment/Decrement 
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x = 10;
+    int y;
+    y = ++x;
+    cout << x << endl
+         << y;
+    return 0;
+}
+//Post Increment/Decrement
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x = 10;
+    int y;
+    y = x++;
+    cout << x << endl
+         << y;
     return 0;
 }
