da4858054029059ed8313b514dfe00c14919d676
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-27 00:24:53 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,21 @@
+// if else
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   int x;
+   cout<<"enter number: ";
+   cin>>x;
+   if(x%2==0)
+   {
+    cout<<endl<<"even number";
+   }
+   else{
+    cout<<endl<<"odd number";
+   }
+}
+
+
