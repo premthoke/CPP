b386f7fc18e7243e20367c0941720160c83d2675
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-28 23:07:34 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -43,3 +43,21 @@ int main()
         cout << "FAil !";
     }
 }
+
+
+//Calc
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int n1;
+  cin>>n1;
+  char op;
+  cin>>op;
+  int n2;
+  cin>>n2;
+  if(op=='+') cout<<n1+n2;
+  if(op=='-') cout<<n1-n2;
+  if(op=='*') cout<<n1*n2;
+  if(op=='/') cout<<n1/n2;
+}
