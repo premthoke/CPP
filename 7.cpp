94589b7c54e25a51384e57882a34d6fb97c76096
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-29 20:10:01 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -15,3 +15,46 @@ int main()
     }
     return 0;
 }
+
+//Factor & Highest factor
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int hf;
+    cout << "enter number!";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " ";
+            hf = i;
+        }
+    }
+    cout << "Highest Factor : " hf;
+}
+
+//Prime Or Composite
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    bool flag = true;
+    cout << "enter number!";
+    cin >> n;
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (flag == true)
+        cout << "Prime NUmber";
+    else
+        cout << "composite number:";
+}
