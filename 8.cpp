4f38144fbddb6fafeabf859e21fd6c473a03035b
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-29 20:16:39 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -16,3 +16,40 @@ int main()
     }
     cout << "count of digits : " << count;
 }
+
+//Sum of digits
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter number!";
+    cin >> n;
+    int sum;
+    while (n != 0)
+    {
+        int ld = n % 10;
+        n = n / 10;
+        sum += ld;
+    }
+    cout << sum;
+}
+
+//Reverse of digits
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter number!";
+    cin >> n;
+    int r;
+    while (n != 0)
+    {
+        int ld = n % 10;
+        r *= 10;
+        n /= 10;
+        r += ld;
+    }
+    cout << r;
+}
