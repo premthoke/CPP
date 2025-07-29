41aa581984f28e36f1f201e9dbf631b7a621c9f5
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-29 14:21:15 +0530

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,17 @@
+//loops
+//for loop
+
+//Table
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter number" << endl;
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << n * i << endl;
+    }
+    return 0;
+}
