12f5349ef8730a66b3d7df5a2b759c3d609d6d27
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-30 22:23:20 +0530

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -57,3 +57,23 @@ int main()
     }
     return 0;
 }
+
+
+//1-12-123-1234
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
