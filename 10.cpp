fbaa9d26ec6c61ec8ad0bf63cf7f30e45fe12b73
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-31 22:51:12 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -61,4 +61,22 @@ int main()
     return 0;
 }
 
-
+//1 01 101 0101
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if((i+j)%2==0) cout<<1<<" ";
+            else cout<<0<<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
