1104999c84832e96e6b883fc37b0e0d24f8cdac6
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-07-31 22:38:15 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,1 +1,64 @@
+//Trangle
+
+//1-12-123-1234
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//1-13-135-1357 (odd nums)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<2*j-1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//1 23 456 78910
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    int a=1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout <<a<<" ";
+            a++
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
 
