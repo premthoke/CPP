3265ca2b132d6a52f80970e8b939d37e35515e42
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-02 17:08:29 +0530

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -10,3 +10,47 @@ int main() {
     funname();
     return 0;
 }
+
+//Function in Function (return;)
+#include <iostream>
+using namespace std;
+void usa()
+{
+    cout << "hello usa" << endl;
+    return;
+}
+void india()
+{
+    cout << "hello india" << endl;
+    usa();
+}
+int main()
+{
+    cout << "hello world" << endl;
+    india();
+    return 0;
+}
+
+//sum
+#include <iostream>
+using namespace std;
+void sum(int a, int b)
+{
+    cout << a + b;
+}
+int main()
+{
+    sum(3, 5);
+}
+
+//return func
+#include <iostream>
+using namespace std;
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    cout << sum(9, 10);
+}
