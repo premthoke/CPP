5f8e2714c40e3d4fe96d850d71bf9a069203ba3b
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-05 23:41:36 +0530

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -31,3 +31,20 @@ int main() {
     cout<<x;
     return 0;
 }
+
+//SWAP using pointer
+#include <iostream>
+using namespace std;
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b= temp;
+}
+int main() {
+    int x=3;
+    int y=6;
+    cout<<x<<" "<<y<<endl;
+    swap(&x,&y);
+    cout<<x<<" "<<y<<endl;
+    return 0;
+}
