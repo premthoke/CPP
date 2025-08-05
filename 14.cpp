0ec1dd04be4d6caa13c4762cfcbf9f9ae569dce1
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-05 19:49:25 +0530

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,34 @@
+//Pointer
+
+//pass by value
+#include <iostream>
+using namespace std;
+void change(int a)
+{
+    a = 22;
+}
+int main()
+{
+    int x = 5;
+    change(x);
+    cout << x;
+    return 0;
+}
+
+
+//swap - pass by reference
+#include <iostream>
+using namespace std;
+void swap(int &x,int &y){
+    int temp=x;
+    x=y;
+    y=temp;
+}
+int main() {
+    int x=12;
+    int y=45;
+    cout <<x<<" "<<y<<endl;
+    swap(x,y);
+      cout <<x<<" "<<y<<endl;
+    return 0;
+}
