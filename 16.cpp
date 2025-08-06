49b7d57b3ca05f6284142056844ac0da351c75d3
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-06 23:03:37 +0530

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,19 @@
+//Recursion
+
+//function calling itself
+#include <iostream>
+using namespace std;
+void print(int n)
+{
+    if (n == 0)
+        return;
+    cout << n << endl;
+    print(n - 1);
+}
+int main()
+{
+    int n;
+    cout << "enter n";
+    cin >> n;
+    print(n);
+}
