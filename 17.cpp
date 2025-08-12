39ed50325d21c5fba897f29fe834f3c59d3e13e2
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-12 23:30:43 +0530

    Create 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,16 @@
+// print 1 to n without exta parameter
+
+#include <iostream>
+using namespace std;
+void print(int n){
+    if(n==0) return; //base case
+    print(n-1);      //call
+    cout<<n<<endl;   //work
+}
+int main() {
+    int n;
+    cout<<"enetr n : ";
+    cin>>n;
+    print(n);
+    return 0;
+}
