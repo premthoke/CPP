7f25dbd76e2ca6290dd93dae143c71044760ee5c
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-12 23:47:55 +0530

    Update 17.cpp

--- 17.cpp
+++ 17.cpp
@@ -29,3 +29,21 @@ int main() {
     cout<<sum(n);
     return 0;
 }
+
+//function which calculates 'a' raised to power 'b' using recursion
+#include <iostream>
+using namespace std;
+int power(int a,int b){
+    if(b==0) return 1;    
+    return a * power(a,b-1);
+}
+int main() {
+    int a;
+    cout<<"enetr a : "<<endl;
+    cin>>a;
+     int b;
+    cout<<"enetr b : "<<endl;
+    cin>>b;
+    cout<<power(a,b);
+    return 0;
+}
