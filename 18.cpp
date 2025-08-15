96d505400fae1c7a9d2024d770c136e951108ed9
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-15 23:18:05 +0530

    Update 18.cpp

--- 18.cpp
+++ 18.cpp
@@ -13,3 +13,22 @@ cin>>n;
 fibo(n);
 cout<<fibo(n);
 }
+
+//Power function - Logarithmic
+#include<iostream>
+using namespace std;
+int power(int a, int b) {
+    if(b == 0) return 1;
+    int half = power(a, b/2);
+    if(b % 2 == 0) return half * half;
+    else return a * half * half;
+}
+int main() {
+    int a, b;
+    cout << "enter base: ";
+    cin >> a;
+    cout << "enter exponent: ";
+    cin >> b;
+    cout << power(a, b);
+}
+
