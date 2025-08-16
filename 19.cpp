aa6cc0c1ba12e85f372d581de59e06379c57ebdf
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-16 23:51:56 +0530

    Update 19.cpp

--- 19.cpp
+++ 19.cpp
@@ -1,1 +1,12 @@
 //Arrays
+
+//basic declaration and printing of array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[4] = {7, 3, 9, 2};
+    for (int i = 0; i < 4; i++) {   // use < instead of <=
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
