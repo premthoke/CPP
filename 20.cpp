444445a6858dbf9a1de3b9d322653bfef2100529
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-17 00:47:50 +0530

    Update 20.cpp

--- 20.cpp
+++ 20.cpp
@@ -14,3 +14,21 @@ int main() {
     }
     return 0;
 }
+
+//WAP for an array arr contains n elements then check if the given array is palindrome or not
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1, 2, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bool isPalindrome = true;
+    for(int i = 0; i < n/2; i++) {
+        if(arr[i] != arr[n-1-i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if(isPalindrome) cout << "Palindrome";
+    else cout << "Not Palindrome";
+    return 0;
+}
