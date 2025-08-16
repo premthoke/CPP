b1aa497398c21beabf8862fcc7764dde49274bfa
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-17 00:42:30 +0530

    Update 19.cpp

--- 19.cpp
+++ 19.cpp
@@ -52,3 +52,40 @@ int main() {
     }
     return 0;
 }
+
+//count the number of elements in given array greater than a given number x
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[5] = {7, 2, 3, 6, 8};
+    int x, count = 0;
+    cout << "enter x: ";
+    cin >> x;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] > x) count++;
+    }
+    cout << "count = " << count;
+    return 0;
+}
+
+//find the second largest element of given array
+#include<iostream>
+#include<climits>   // for INT_MIN
+using namespace std;
+int main() {
+    int arr[6] = {12, 7, 25, 18, 25, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int first = INT_MIN, second = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > first) {
+            second = first;
+            first = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+    if(second == INT_MIN) cout << "No second largest element";
+    else cout << "second largest = " << second;
+    return 0;
+}
