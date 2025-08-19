675b726b1dad28be5b2a202e4c6dcc00446143ad
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-19 21:56:45 +0530

    Update 22.cpp

--- 22.cpp
+++ 22.cpp
@@ -30,3 +30,40 @@ int arr[5][5];
     }
     return 0;
 }
+
+//WAP to add two matrices
+#include <iostream>
+using namespace std;
+int main() {
+ int arr1[4][2]={{1,2},{3,4},{5,6},{7,8}};
+  int arr2[4][2]={{1,2},{3,4},{5,6},{7,8}};
+  int arr3[4][2];
+   for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+          arr3[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+       for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+            cout<<arr3[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+//Find the max element in given matrix
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+ int arr1[4][2]={{1,2},{3,4},{5,6},{7,8}};
+ int mx = INT_MIN;
+   for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+          mx=max(mx,arr1[i][j]);
+        }
+    }
+       cout<<mx;
+    return 0;
+}
