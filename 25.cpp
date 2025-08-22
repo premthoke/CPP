0759603cf3613df7c2b2c2b1476b7bc65c311987
Prem Thoke <139491255+premthoke@users.noreply.github.com>
2025-08-22 11:53:20 +0530

    Update 25.cpp

--- 25.cpp
+++ 25.cpp
@@ -1,1 +1,35 @@
 //programs
+
+//WAP to count the vowels in string
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s = "prem is good boy";
+    int count;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
+
+//input a string and update all even positions in the string to character 'a' and return it.
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s;
+	cout<<"enter the string"<<endl;
+	getline(cin,s);
+	cout<<s<<endl;
+	for(int i=0; i<s.length(); i++) {
+		if(i%2) {
+			s[i]='a';
+		}
+	}
+	cout<<s<<endl;
+	return 0;
+}
